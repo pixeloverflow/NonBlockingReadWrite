f0965954ddfdd5157ba6b33263d35b5555333ddc
pixeloverflow <bartek.dabrowski1.11@gmail.com>
2018-03-16 15:06:30 +0100

    Added parallelism to both applications. Made runnable.

--- src/Reader.java
+++ src/Reader.java
@@ -1,7 +1,7 @@
 import java.nio.IntBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Reader {
@@ -9,18 +9,22 @@ public class Reader {
     private FileChannel channel;
     private IntBuffer humanReadableFormat;
     private int operationResult = 0;
+    private boolean flag = false;
 
-    public Reader(Path _path, MappedByteBuffer buffer) throws Exception {
-        buff = buffer;
-        channel = FileChannel.open(_path, StandardOpenOption.READ);
+    public Reader(String _path, String bufferSize) throws Exception {
+        channel = FileChannel.open(Paths.get(_path), StandardOpenOption.READ, StandardOpenOption.WRITE);
+        buff = channel.map(FileChannel.MapMode.READ_WRITE, 0, Integer.parseInt(bufferSize)*4);
+        humanReadableFormat = buff.asIntBuffer();
+        humanReadableFormat.put(0,0);
     }
 
-    public void readFromFile() throws Exception {
-        channel.read(buff);
-        channel.close();
-        buff.flip();
-        humanReadableFormat = buff.asIntBuffer();
-        calc();
+    private void readFromFile() throws Exception {
+        if(humanReadableFormat.get(0) == 0) {
+            calc();
+            humanReadableFormat.rewind();
+            System.out.println("Read : " + result());
+            resetResult();
+        }
     }
 
     private void calc(){
@@ -29,7 +33,24 @@ public class Reader {
         }
     }
 
-    public int result(){
+    private int result(){
         return operationResult;
+    }
+
+    private void resetResult(){
+        operationResult = 0;
+        humanReadableFormat.put(0, 1);
+    }
+
+    private void start() throws Exception {
+        while(true){
+            readFromFile();
+            Thread.sleep(100);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader rd = new Reader(args[0], args[1]);
+        rd.start();
     }
 }
--- src/Writer.java
+++ src/Writer.java
@@ -1,39 +1,55 @@
-import java.nio.IntBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Writer {
     private MappedByteBuffer buff;
     private FileChannel channel;
     private int[] data;
-    private int expectedResult = 0;
     private Path path;
 
-    public Writer(Path _path, MappedByteBuffer buffer, int[] toWrite) throws Exception {
-        path = _path;
-        channel = FileChannel.open(path, StandardOpenOption.WRITE);
-        buff = buffer;
-        data = toWrite;
+    public Writer(String _path, String rawData) throws Exception {
+        path = Paths.get(_path);
+        channel = FileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.READ);
+        String[] strings = rawData.split(";");
+        data = new int[strings.length];
+        buff = channel.map(FileChannel.MapMode.READ_WRITE, 0, (data.length+1)*4);
+        for (int i = 0; i <= strings.length-1 ;++i) {
+            data[i] = Integer.parseInt(strings[i]);
+        }
     }
 
-    public void writeToFile() throws Exception {
-        for (int i = 0 ; i <= data.length-1 ;++i){
-            buff.putInt(data[i]);
-            expectedResult += data[i];
-        }
-        IntBuffer bufff = buff.asIntBuffer();
-        while (bufff.hasRemaining()){
-            System.out.println(bufff.get());
-        }
-        channel.write(buff);
-        buff.force();
-        channel.close();
-        buff.rewind();
+    private int random(){
+        return (int)(Math.random()*10);
     }
 
-    public int getExpectedResult(){
-        return expectedResult;
+    private void start() throws Exception {
+        while (true) {
+            if (buff.getInt(0)==1) {
+                System.out.print("Adding :");
+                for (int i = 1; i <= data.length - 1; ++i) {
+                    int r = random();
+                    if (r == 1) r = 0;
+                    System.out.print(" " + r);
+                    data[i] = r;
+                }
+                System.out.println();
+
+                for (int i = 1; i <= data.length - 1; ++i) {
+                    buff.putInt(data[i]);
+                }
+
+                buff.putInt(0, 0);
+                buff.rewind();
+            }
+            Thread.sleep(100);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Writer wr = new Writer(args[0], args[1]);
+        wr.start();
     }
 }
